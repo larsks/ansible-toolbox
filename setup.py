39988f8c876f19a3535b360661df05d3d303408d
Lars Kellogg-Stedman <lars@redhat.com>
2015-10-19 15:48:59 -0400

    fix bug in verbose arg, inc version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='ansible-role',
       author='Lars Kellogg-Stedman',
       author_email='lars@redhat.com',
       url='https://github.com/larsks/ansible-role',
-      version='0.1',
+      version='0.2',
       scripts = [
           'ansible-role',
       ]
