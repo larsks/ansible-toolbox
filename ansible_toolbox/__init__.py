b4c9f25076064cb9ec43e4468fc51c7b69ec9783
Lars Kellogg-Stedman <lars@redhat.com>
2016-05-06 08:12:21 -0400

    packaging fixes
    - typo in MANIFEST.in
    - added setup.cfg
    - better version handling

---
+++ ansible_toolbox/__init__.py
@@ -0,0 +1,2 @@
+__version__ = '0.2'
+__author__ = 'Lars Kellogg-Stedman <lars@oddbit.com>'
--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
+from ansible_toolbox import __version__ as version
 
 with open('requirements.txt') as fd:
     setup(name='ansible-toolbox',
           author='Lars Kellogg-Stedman',
-          author_email='lars@redhat.com',
+          author_email='lars@oddbit.com',
           url='https://github.com/larsks/ansible-role',
-          version='1.0',
+          version=version,
           install_requires=fd.readlines(),
           packages=find_packages(),
           package_data={'ansible_toolbox': [
